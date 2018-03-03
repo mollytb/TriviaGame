9f1bd92c40d0b01d7c15cd96c3c0cfc0a9a06a92
Molly Burns <Molly@Kathryns-MBP.attlocal.net>
2018-03-03 01:52:32 -0500

    sort of works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+    var right = 0;
+    var wrong = 0;
+
+
 //js attached
 console.log(12);
 //hide questions
@@ -7,34 +11,67 @@ $("#div2").hide();
 $("#start").on("click",init);
 function init(){
     $("#start").hide();
-  
-    $("#question").text("What year was Saint Mary's College founded")
+    right = 0;
+    wrong = 0;
+    question1();
+}
+
+//attach questions to the hidden div
+    function question1(){
+    $("#question").text("What year was Saint Mary's College founded?")
     $("#btn1").text("1844");
     $("#btn2").text("1992");
     $("#btn3").text("1895");
     $("#btn4").text("1888");
     $("#div2").show();
-    setTimeout(function() { question2(); }, 1000 * 10);
-    
-
+    setTimeout(function() { question2(); }, 1000 * 3);
+    $("#btn1").click(function(){
+        right ++;
+        clearTimeout;
+        });
+    $("#btn2").click(function(){
+        wrong ++;
+        clearTimeout;
+        });
+    $("#btn3").click(function(){
+        wrong ++;
+        clearTimeout;
+        });
+    $("#btn4").click(function(){
+        wrong ++;
+        clearTimeout;
+        });       
+  
     }
 
 
-  });
-
-
-//attach questions to the hidden div
 
 //question 2
 function question2(){
 
 $("#question").text("Who was the first SMC student to become president of the college?")
-$("#btn1").text("Madaleva");
+$("#btn1").text("potter");
 $("#btn2").text("Mooney");
-$("#btn3").text("Potter");
+$("#btn3").text("Madaleva");
 $("#btn4").text("Burns");
 $("#div2").show();
-    setTimeout(function() { question3(); }, 1000 * 10);
+    setTimeout(function() { question3(); }, 1000 * 3);
+    $("#btn3").click(function(){
+        right ++;
+        clearTimeout;
+        });
+    $("#btn2").click(function(){
+        wrong ++;
+        clearTimeout;
+        });
+    $("#btn1").click(function(){
+        wrong ++;
+        clearTimeout;
+        });
+    $("#btn4").click(function(){
+        wrong ++;
+        clearTimeout;
+        });
     
 }
 //question 3
@@ -45,7 +82,23 @@ $("#btn2").text("1965");
 $("#btn3").text("1895");
 $("#btn4").text("1983");
 $("#div2").show();
-    setTimeout(function() { question4(); }, 1000 * 10);
+    setTimeout(function() { question4(); }, 1000 * 3);
+    $("#btn1").click(function(){
+        right ++;
+        clearTimeout;
+        });
+    $("#btn2").click(function(){
+        wrong ++;
+        clearTimeout;
+        });
+    $("#btn3").click(function(){
+        wrong ++;
+        clearTimeout;
+        });
+    $("#btn4").click(function(){
+        wrong ++;
+        clearTimeout;
+        });
     
 }
 //question 4
@@ -56,22 +109,78 @@ $("#btn2").text("1992");
 $("#btn3").text("2018");
 $("#btn4").text("1888");
 $("#div2").show();
-    setTimeout(function() { question5(); }, 1000 * 10);
-    
+    setTimeout(function() { question5(); }, 1000 * 3);
+    $("#btn3").click(function(){
+        right ++;
+        clearTimeout;
+        });
+    $("#btn2").click(function(){
+        wrong ++;
+        clearTimeout;
+        });
+    $("#btn1").click(function(){
+        wrong ++;
+        clearTimeout;
+        });
+    $("#btn4").click(function(){
+        wrong ++;
+        clearTimeout;
+        });
 }
 //question 5
 function question5(){
-$("#question").text("What year was Saint Mary's charter authorized to include a college")
+$("#question").text("What year was Saint Mary's charter authorized to include a college?")
 $("#btn1").text("1844");
 $("#btn2").text("1908");
 $("#btn3").text("1915");
 $("#btn4").text("1898");
 $("#div2").show();
-    setTimeout(function() { results(); }, 1000 * 10);
+    setTimeout(function() { results(); }, 1000 * 3);
+    $("#btn2").click(function(){
+        right ++;
+        clearTimeout;
+        });
+    $("#btn1").click(function(){
+        wrong ++;
+        clearTimeout;
+        });
+    $("#btn3").click(function(){
+        wrong ++;
+        clearTimeout;
+        });
+    $("#btn4").click(function(){
+        wrong ++;
+        clearTimeout;
+        });
   
 }
 function results(){
+
+    var newDiv = $("<div id='newdiv'>").text("correct " + right);
+    var newDiv2 = $("<div id='newdiv2'>").text("incorrect " + wrong);
     //show # of correct answers
     //show # of incorrect answers
-    $("#div2").text("Game Over")
+    $("#question").hide();
+    $("#btn1").text("Game Over");
+    $("#btn2").text("restart");
+    $("#btn3").hide();
+    $("#btn4").hide();
+    $("#div2").show();
+    $("#div2").append(newDiv);
+    $("#newdiv").append(newDiv2);
+    $("#btn2").on("click", restart);
+
 }
+function restart (){
+    right = 0;
+    wrong = 0;
+    $("#newdiv").empty();
+    $("#newdiv2").empty();
+    $("#start").show();
+    $("#question").show();
+    $("#btn3").show();
+    $("#btn4").show();
+    $("#div2").hide();
+    
+}
+});
